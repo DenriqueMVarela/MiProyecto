06d4a6dfd34f3c8df4f3133d8cf065e73ed88fa0
Usuario-pc <Usuario-pc@DESKTOP-NTJ5PR6>
2018-11-02 08:38:29 -0500

    Cine

---
+++ Cine/src/Admin/Peliculas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Admin;
+
+import java.time.LocalDateTime;
+import javax.swing.Icon;
+
+/**
+ *
+ * @author Usuario-pc
+ */
+public class Peliculas {
+    String Nombre;
+    Icon imagen;
+    LocalDateTime ldt;
+    LocalDateTime in;
+    LocalDateTime fin;
+    int Duración;
+}
